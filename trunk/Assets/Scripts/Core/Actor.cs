f735b7d90ec99b89ba14ee9bf4e9a0c2e9b48f0b
samuel.lapointe1@gmail.com <samuel.lapointe1@gmail.com@1507d330-f3c5-0386-5a8f-361409dbd2f3>
2015-02-19 03:38:11 +0000

    Àdded WeaponEditor.cs

---
+++ trunk/Assets/Scripts/Core/Actor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor : MonoBehaviour 
+{
+	#region Variables
+	private Transform actorTransform;
+	private GameObject actorGameObject;
+	private Renderer actorRenderer;
+	private Rigidbody actorRigidbody;
+	private Collider actorCollider;
+	#endregion
+	
+	public Transform aTransform
+	{
+		get{ return actorTransform? actorTransform: actorTransform = transform; }
+	}
+	
+	public GameObject aGameObject
+	{
+		get{ return actorGameObject? actorGameObject: actorGameObject = gameObject; }
+	}
+	
+	public Renderer aRenderer
+	{
+		get{ return actorRenderer? actorRenderer: actorRenderer = renderer; }
+	}
+	
+	public Rigidbody aRigidbody
+	{
+		get{ return actorRigidbody? actorRigidbody: actorRigidbody = rigidbody; }
+	}
+	
+	public Collider aCollider
+	{
+		get{ return actorCollider? actorCollider: actorCollider = collider; }
+	}
+	
+	protected virtual void Awake()
+	{	
+		actorTransform = aTransform;
+		actorGameObject = aGameObject;
+		actorRenderer = aRenderer;
+		actorRigidbody = aRigidbody;
+		actorCollider = aCollider;
+	}
+	
+	protected virtual void ActorStart()
+	{
+	}
+}
---
+++ trunk/Assets/Scripts/Manager/WeaponManager/WeaponManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponManager : Singleton<WeaponManager> 
+{
+	public static string m_AssetPath = "Assets/Resources/WeaponInfos.asset";
+
+	public static WeaponInfos m_WeaponInfo;
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		m_WeaponInfo = Resources.LoadAssetAtPath<WeaponInfos>(m_AssetPath);
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
