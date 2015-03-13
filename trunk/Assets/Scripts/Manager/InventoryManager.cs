ac08d75ad97d822f13c3fda851c99d283d47c9be
samuel.lapointe1@gmail.com <samuel.lapointe1@gmail.com@1507d330-f3c5-0386-5a8f-361409dbd2f3>
2015-03-13 23:35:51 +0000

    Commit before big changes

---
+++ trunk/Assets/Scripts/Manager/InventoryManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InventoryManager : Singleton<InventoryManager> 
+{
+	public GameObject m_ItemButton;
+
+	protected override void Awake()
+	{
+		base.Awake();
+	}
+	
+	void Start () 
+	{
+	}
+
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(1))
+		{
+			AddItem();
+		}
+	}
+
+	public void AddItem()
+	{
+		GameObject itemGO = (GameObject)Instantiate(m_ItemButton);
+		itemGO.transform.SetParent(RefManager.Instance.m_InventoryContainer, false);
+	}
+}
--- trunk/Assets/Scripts/Manager/RefManager.cs
+++ trunk/Assets/Scripts/Manager/RefManager.cs
@@ -12,10 +12,14 @@ public class RefManager : Singleton<RefManager>
 	public Transform m_CanvasTop;
 	public Transform m_CanvasLeft;
 	public Transform m_CanvasRight;
+	
+	[Header("INVENTORY")]
+	public Transform m_InventoryContainer;
 
 	protected override void Awake()
 	{
 		base.Awake();
+		Application.runInBackground = true;
 	}
 	
 	void Start () 
